ee960847b507a7d08c7513b4c88ba4558aeab6d4
Vinchum <vcchum1@gmail.com>
2024-08-28 02:45:27 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "Vince";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType);
+console.log('myVarType ${myVarType}');
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log('will 11 this one run?');
+    } else {
+        console.log('will 13 this one run?');
+    }console.log('inside the function');
+}
+
+runNow();
+runNow();
