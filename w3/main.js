4e1ba35823356ae37417b7160c71174eaadb8d1e
Vinchum <vcchum1@gmail.com>
2024-09-04 00:18:43 -0700

    working code

--- w3/main.js
+++ w3/main.js
@@ -21,9 +21,35 @@ function determineHouseHoldPts(numberInHousehold) {
 let cardonFootPoints = 0;
 // const numberInHousehold = 3;
 
-
 // global scope
 // forgot to commit for first message
 
 determineHouseHoldPts(3)
 determineHouseHoldPts(4)
+
+// 2. Consider the size of your home:
+// If you have a large house, then add 10 points to your score.
+// If you have a medium-sized house, then add 7 points.
+// If you have a small house, then add 4 points.
+// If you live in an apartment, then add 2 points.
+
+function determineHouseholdSize(sizeOfHousehold) {
+    console.log("Inside the function");
+    if (HouseholdSize === 1) {
+        cardonFootPoints = cardonFootPoints + 10
+    } else if (HouseholdSize === 2) {
+        cardonFootPoints = cardonFootPoints + 7
+    } else if (HouseholdSize === 3) {
+        cardonFootPoints = cardonFootPoints + 4
+    } else if (HouseholdSize === 4) {
+        cardonFootPoints = cardonFootPoints + 2
+    }
+    console.log(`Based on the size of your ${sizeOfHousehold} the points would be ${cardonFootPoints}.`);
+}
+
+cardonFootPoints = 0;
+const sizeOfHousehold = 1;
+const HouseholdSize = 1;
+
+determineHouseholdSize(1)
+determineHouseholdSize(2)
