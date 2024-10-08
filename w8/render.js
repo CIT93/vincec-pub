7a7ee3673a95681bf6f8d9aae5eb3fa0e42f4333
Vinchum <vcchum1@gmail.com>
2024-10-07 17:28:52 -0700

    code from watching solution

--- w8/render.js
+++ w8/render.js
@@ -1,27 +1,23 @@
 const TBL = document.getElementById("tab-data");
+const FORM = document.getElementById("form");
 
 function renderTblHeading() {
-    TBL.innerHTML = "";
+    //TBL.innerHTML = "";
     const table = document.createElement("table");
-    const renderbtnDel = false;
-    if (renderbtnDel) {
-        const thead = document.createElement("thead");
-        const tr = document.createElement("tr");
-        const headingTextArr = ["Name", "HouseHold", "House Size", "Footprint", "Actions"];
-        headingTextArr.forEach(function (text) {
-            const th = document.createElement("th");
-            th.textContent = text;
-            tr.appendChild(th);
-        });
-            thead.appendChild(tr);
-            table.appendChild(thead);
-        }
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const headingTextArr = ["Name", "HouseHold", "House Size", "Footprint", "Actions"];
+    headingTextArr.forEach(function (text) {
+        const th = document.createElement("th");
+        th.textContent = text;
+        tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    table.appendChild(thead);
     return table
 }
 
-// I think I did this part backwards or something but headings do not show up on submit 
-
-function renderTblBtn(index, data) {
+function renderTblBtn(obj, index, data) {
     const td = document.createElement("td");
     const btnEdit = document.createElement("button");
     const btnDel = document.createElement("button");
@@ -30,25 +26,19 @@ function renderTblBtn(index, data) {
     td.appendChild(btnEdit);
     td.appendChild(btnDel);
     btnDel.addEventListener('click', function (e) {
-        console.log('Hello from inside the delete button');
-        console.log(e)
         data.splice(index, 1);
         renderTbl(data);
     })
     btnEdit.addEventListener('click', function (e) {
-        const editForm = document.getElementById('edit-form');
-        editForm.elements['Name'].value = obj.name;
-        editForm.elements['HouseHold'].value = obj.houseHold;
-        editForm.elements['House Size'].value = obj.houseSize;
-        editForm.elements['Footprint'].value = obj.footPrint;
-
-        editForm.dataset.index = index;
-        editForm.style.display = 'block';
+        FORM[1].value = obj.firstName;
+        FORM[2].value = obj.lastName;
+        FORM[3].value = obj.houseM;
+        FORM[4].value = obj.houseS;
+        data.splice(index, 1);
+        renderTbl(data);
     });
     return td;
 }
-
-// getting null on editform elements not sure how to correct this
 
 function renderTblBody(data) {
     const tbody = document.createElement("tbody");
@@ -62,7 +52,7 @@ function renderTblBody(data) {
                 tr.appendChild(td);
             }
         }
-        const td = renderTblBtn(index, data);
+        const td = renderTblBtn(obj, index, data);
         tr.appendChild(td);
         tbody.appendChild(tr);
     });
@@ -70,12 +60,14 @@ function renderTblBody(data) {
 }
 
 function renderTbl(data) {
-    const table = renderTblHeading();
-    const tbody = renderTblBody(data);
-    table.appendChild(tbody);
-    TBL.appendChild(table);
+    TBL.innerHTML = "";
+    if(data.length !== 0){
+        const table = renderTblHeading();
+        const tbody = renderTblBody(data);
+        table.appendChild(tbody);
+        TBL.appendChild(table);
+    } 
+
 };
 
 export { renderTbl };
-
-// getting a reference error at line 56, not sure what should be there instead of copydata.
