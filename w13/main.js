1bfd7ff284560e12404ad8cad1a8aaba9eaab2e7
Vinchum <vcchum1@gmail.com>
2024-11-08 00:15:24 -0800

    my code after watching solution

--- w13/main.js
+++ w13/main.js
@@ -1,51 +1,36 @@
 const FORM = document.getElementById('form');
 const OUT = document.getElementById('output');
 
-function updateDOM(message, el) {
+const updateDOM = (message, el = "h3") => {
     const newEl = document.createElement(el)
     newEl.textContent = message
     OUT.appendChild(newEl)
 }
 
 const startWorkout = (type, reps, time, fn) => {
+    fn(`Start ${type} <> Goal reps is ${reps} <> complete in ${time} min!`, "p")
     return new Promise((resolve, reject) => {
-        fn(`Start ${type} <> Goal reps is ${reps} <> complete in ${time} min!`, "p")
-        setTimeout(() => {
-            resolve(`Stop ${type}`, "h1")
-        }, time * 1000)
+        if (time === 0) {
+            reject(`Error on Time selection`)
+        } else {
+            setTimeout(() => {
+                resolve(`Stop ${type}`)
+            }, time * 1000)
+        }
     })
 }
 
-const startWorkoutIcon = (type, reps, time, fn) => {
-return new Promise (function(resolve, reject) {
-    setTimeout(() => {
-        switch(startWorkout){
-            case 'Finished':
-                resolved('done')
-                break
-            case 'Not finished':
-                resolved('not done')
-            default:
-                reject('No Icon Found')
-        }
-    }, time * 1000)
-})
+const onError = (error) => {
+    updateDOM(error, "h2")
 }
-
-function onFinish() {
-    console.log(`${type} finished`)
-}
-
-startWorkout()
-.then(startWorkout)
-.then(onFinish)
-.catch()
 
 FORM.addEventListener('submit', e => {
     e.preventDefault();
     const type = e.target.type.value
-    const reps = parseFloat (e.target.reps.value)
+    const reps = parseFloat(e.target.reps.value)
     const time = parseFloat(e.target.time.value)
     startWorkout(type, reps, time, updateDOM)
+        .then(updateDOM)
+        .catch(onError)
     FORM.reset()
 })
