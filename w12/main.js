18b12433ad4b2e9e0d90d3c12734a4febe4f42f9
Vinchum <vcchum1@gmail.com>
2024-11-07 17:21:53 -0800

    updating code from resolution for week 13

--- w12/main.js
+++ w12/main.js
@@ -1,53 +1,24 @@
-const FORM = document.getElementById("form");
-const OUTPUT = document.getElementById("output");
-const asyncData = []; 
+const FORM = document.getElementById('form');
+const OUT = document.getElementById('output');
 
-function getExercise() {
-    console.log('Exercise')
-    const exercise = ''
-    console.log('Exercised')
-    return exercise
+function updateDOM(message, el) {
+    const newEl = document.createElement(el)
+    newEl.textContent = message
+    OUT.appendChild(newEl)
 }
 
-function getReps(reps) {
-    console.log('2')
-    return reps
+const startWorkout = (type, reps, time, fn) => {
+    fn(`Start ${type} <> Goal reps is ${reps} <> complete in ${time} min!`, "p")
+    setTimeout(() => {
+        fn(`Stop ${type}`, "h1")
+    }, time * 1000)
 }
 
-function getMinutes(minutes) {
-    console.log('5')
-    return minutes
-}
-
-function start(exercise, reps, minutes) {
-    const typeExercise = getExercise(exercise)
-    const targetReps = getReps(reps)
-    const timeMinutes = getMinutes(minutes)
-    asyncData.push({
-        typeExercise: exercise,
-        targetReps: reps,
-        timeMinutes: minutes,
-    })
-}
-
-function displayOutput() {
-    for (let obj of asyncData) {
-        console.log(obj)
-        const output = document.getElementById("output");
-        const newH2 = document.createElement("h2");
-        newH2.textContent = `Start ${obj.typeExercise} doing ${obj.targetReps} reps in ${obj.timeMinutes} minute(s).`;
-        output.appendChild(newH2);
-    }
-}
-
-FORM.addEventListener('submit', function(e) {
+FORM.addEventListener('submit', e => {
     e.preventDefault();
-    const typeExercise = FORM.exercise.value;
-    const targetReps = FORM.reps.value;
-    const timeMinutes = FORM.minutes.value;
-    start(typeExercise, targetReps, timeMinutes);
-    displayOutput();
-   setTimeout(() => {
-    OUTPUT.textContent = "Good Job!";
-   }, 3000);
-});
+    const type = e.target.type.value
+    const reps = parseFloat (e.target.reps.value)
+    const time = parseFloat(e.target.time.value)
+    startWorkout(type, reps, time, updateDOM)
+    FORM.reset()
+})
