7c6db7c50949074a333b4be74cbbd29a28cc067c
Vince Chum <vcchum1@gmail.com>
2025-09-04 15:54:53 -0700

    initial attempt at greeting generator

---
+++ w4/app.js
@@ -0,0 +1,5 @@
+// Create the form in the html page, create elements for event listeners in app.js for the form to use. 
+// Make sure event listeners don't prefire before prompted, and have h2 activated with event listeners.
+
+const instantGreetingForm = document.getElementById('instantGreetingForm');
+
